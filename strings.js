e735567529cc5fd304845ab2b220486c88891d72
Adam Wieckert <wiec1369@gmail.com>
2018-02-26 21:13:28 -0600

    Finished 4th Challenge in strings.js

--- strings.js
+++ strings.js
@@ -18,3 +18,15 @@ console.log("string", string);
 var DNA = "GCATCATTACGTC";
 var RNA = DNA.replace(/T/g, 'U');
 console.log("RNA:", RNA);
+
+var animal = "ALLIGator";
+
+if(animal === "Alligator"){
+    console.log("animal: ", "wide");
+} else {
+    console.log("small");
+}
+
+if(animal.toLowerCase() === "alligator"){
+    console.log("animal: ", "wide");
+}
