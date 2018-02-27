b9fb71bc5e68a79e55ea0fbda28f2a1cbe412ae7
Adam Wieckert <wiec1369@gmail.com>
2018-02-26 20:30:48 -0600

    Finished second challenge

--- strings.js
+++ strings.js
@@ -3,3 +3,14 @@
 var numOfSandwhiches = 4 * 2;
 
 console.log("numOfSandwiches", numOfSandwhiches);
+
+var cat = "fluffly"
+
+console.log(cat.length);
+
+var name = "batman"
+
+
+// Challenge 2
+var string = "Hello " + name + " how are you doing today.";
+console.log("string", string);
